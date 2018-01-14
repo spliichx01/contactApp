6432054dad6881d94e784652e1f970ff703daf79
spliichx01 <chucaboj@yahoo.com>
2018-01-14 22:32:26 +0100

    javascript proj

---
+++ test.js
@@ -0,0 +1,21 @@
+window.$onclick = function(target, type, cb){
+
+	target.addEventListener(type, cb, false);
+}
+
+var CORE = (function(){
+
+
+	var modeules = {};
+
+	function addModule(module_id, mod){
+
+		modules[module_id] = mod;
+	}
+
+	function registerEvents
+
+
+
+
+})();
